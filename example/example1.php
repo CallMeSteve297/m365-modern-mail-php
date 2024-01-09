bfcdb6c61d059c33276d5db0f69671e8e97e01e8
Steve at ITFS <122524858+itfs-steve@users.noreply.github.com>
2024-01-09 16:53:39 +0000

    Create example1.php

---
+++ example/example1.php
@@ -0,0 +1,46 @@
+<?php
+include "./graph_Token.php";
+include "./graph_MailSend.php";
+
+$token = new graph_GetToken();
+$token->client_secret = 'redacted';
+$token->client_id = 'redacted';
+$token->tenant_id = 'redacted';
+
+
+// HTML Example
+
+$recipients = array(
+    "user1@example.com",
+    "user2@example.com"
+);
+
+$htmlContent = file_get_contents("test.html");
+$email = new graph_sendMail();
+$email->token = $token->fetch("token");
+$email->sender = "user@example.com";
+$email->subject = "My HTML Message";
+$email->bodytype = "html";
+$email->body = $htmlContent;
+$email->recipients = $recipients;
+
+echo $email->send();
+
+
+// Plaintext Example 
+
+$recipients1 = array(
+    "user3@example.com"
+);
+
+$email1 = new graph_sendMail();
+$email1->token = $token->fetch("token");
+$email1->sender = "user@example.com";
+$email1->subject = "My Plaintext Message";
+$email1->bodytype = "text";
+$email1->body = "Boom shacka lacka!";
+$email1->recipients = $recipients1;
+
+echo $email1->send();
+
+?>
